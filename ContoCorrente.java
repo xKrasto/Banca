d612930e0c3df625a15d41cd033178b463ca8a81
Alex <32963931+xKrasto@users.noreply.github.com>
2017-11-01 23:22:38 +0100

    Aggiunta la DBAD Public License

--- ContoCorrente.java
+++ ContoCorrente.java
@@ -1,4 +1,34 @@
+/*
+*# DON'T BE A DICK PUBLIC LICENSE
+*
+*> Version 1.1, December 2016
+*
+*> Copyright (C) [2017] [Alexandro, xKrasto]
+*
+*Everyone is permitted to copy and distribute verbatim or modified
+*copies of this license document.
+*
+*> DON'T BE A DICK PUBLIC LICENSE
+*> TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+*
+*1. Do whatever you like with the original work, just don't be a dick.
+*
+*   Being a dick includes - but is not limited to - the following instances:
+*
+* 1a. Outright copyright infringement - Don't just copy this and change the name.
+* 1b. Selling the unmodified original with no work done what-so-ever, that's REALLY being a dick.
+* 1c. Modifying the original work to contain hidden harmful content. That would make you a PROPER dick.
+*
+*2. If you become rich through modifications, related works/services, or supporting the original work,
+*share the love. Only a dick would make loads off this work and not buy the original work's
+*creator(s) a pint.
+*
+*3. Code is provided with no warranty. Using somebody else's code and bitching when it goes wrong makes
+*you a DONKEY dick. Fix the problem yourself. A non-dick would submit the fix back.
+ */
+
 package banca;
+
 import java.util.Date;
 
 public class ContoCorrente {
@@ -25,7 +55,7 @@ public class ContoCorrente {
     public String getId() {
         return id;
     }
-    
+
     public double getSaldo() {
         return saldo;
     }
@@ -38,11 +68,9 @@ public class ContoCorrente {
         return importoUM;
     }
 
-    
-    
     public double versamento() {
         double importo = Banca.reqImporto();
-        this.dataUM=Banca.reqData();
+        this.dataUM = Banca.reqData();
         if (controlloImporto(importo, true)) {
             this.importoUM = importo;
             this.saldo += importo;
@@ -52,6 +80,7 @@ public class ContoCorrente {
         }
         return -1;
     }
+
     public double versamento(double importo, Date data) {
         this.importoUM = importo;
         this.dataUM = data;
@@ -59,9 +88,10 @@ public class ContoCorrente {
         return saldo;
 
     }
+
     public double prelevamento() {
         double importo = Banca.reqImporto();
-        this.dataUM=Banca.reqData();
+        this.dataUM = Banca.reqData();
         if (controlloImporto(Banca.reqImporto(), false)) {
             this.importoUM = importo;
             this.saldo -= importo;
@@ -71,6 +101,7 @@ public class ContoCorrente {
         }
         return -1;
     }
+
     public double prelevamento(double importo, Date data) {
         this.importoUM = importo;
         this.dataUM = data;
