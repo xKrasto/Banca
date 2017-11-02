d38ac9bda28cac62a6561ac69e1960c1eeeae27a
Alex <32963931+xKrasto@users.noreply.github.com>
2017-11-02 20:53:54 +0100

    Bug Fix

--- Banca.java
+++ Banca.java
@@ -27,28 +27,23 @@
 *you a DONKEY dick. Fix the problem yourself. A non-dick would submit the fix back.
  */
 package banca;
-
 import java.util.Date;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-
 /**
  *
  * @author Alex
  */
 public class Banca {
-
     public static String reqId() {
         String id;
         id = "0";
         boolean done;
-
         InputStreamReader inp = new InputStreamReader(System.in);
         BufferedReader tastiera = new BufferedReader(inp);
-
         do {
             try {
                 System.out.println("Inserisci il numero di conto corrente");
@@ -65,14 +60,11 @@ public class Banca {
         } while (!done);
         return id;
     }
-
     public static double reqImporto() {
         double importo = 0;
         boolean done;
-
         InputStreamReader inp = new InputStreamReader(System.in);
         BufferedReader tastiera = new BufferedReader(inp);
-
         do {
             try {
                 System.out.println("Inserisci l'importo");
@@ -82,36 +74,29 @@ public class Banca {
                 } else {
                     done = true;
                 }
-
             } catch (IOException e) {
                 System.err.println("Errore nell'inserimento dell'importo!");
                 done = false;
-
             }
-
         } while (!done);
         return importo;
     }
-
     public static void stampaTutto(ContoCorrente c) {
         DateFormat def = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         System.out.println("Id Conto: " + c.getId());
         System.out.println("Saldo conto: " + c.getSaldo());
         System.out.println("Importo ultimo movimento: " + c.getImportoUM());
         System.out.println("Data ultimo movimento: " + def.format(c.getDataUM()));
-
     }
-
     public static Date reqData() {
         Date data = new Date();
         return data;
     }
-
     public static void main(String[] args) {
-        // TODO code application logic here
+        // 
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader tastiera = new BufferedReader(input);
-
+        //    
         ContoCorrente c1, c2;
         String id;
         double risultato;
@@ -119,26 +104,27 @@ public class Banca {
         c1 = new ContoCorrente();
         System.out.println("-----Conto 2----");
         c2 = new ContoCorrente(Banca.reqId());
-
         do {
+            System.out.println("Importo da versare nel conto 1!");
             risultato = c1.versamento();
         } while (risultato == -1);
         do {
+            System.out.println("Importo da versare nel conto 2");
             risultato = c2.versamento();
         } while (risultato == -1);
         do {
+            System.out.println("Importo da prelevare dal conto 1");
             risultato = c1.prelevamento();
         } while (risultato == -1);
         do {
+            System.out.println("Importo da prelevare dal conto 2");
             risultato = c2.prelevamento();
         } while (risultato == -1);
         do {
+            System.out.println("Importo da versare nel conto 1");
             risultato = c1.versamento();
         } while (risultato == -1);
-
-        stampaTutto(c2);
         stampaTutto(c1);
-
+        stampaTutto(c2);
     }
-
 }
