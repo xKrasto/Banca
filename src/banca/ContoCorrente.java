e071863680a071578e4f76b622846c85c0b54907
Alex <Alex@Alex-sDesktop>
2017-11-02 20:51:54 +0100

    Bug Fix

--- src/banca/ContoCorrente.java
+++ src/banca/ContoCorrente.java
@@ -26,48 +26,35 @@
 *3. Code is provided with no warranty. Using somebody else's code and bitching when it goes wrong makes
 *you a DONKEY dick. Fix the problem yourself. A non-dick would submit the fix back.
  */
-
 package banca;
-
 import java.util.Date;
-
 public class ContoCorrente {
-
     private final String id;
     private double saldo;
     private Date dataUM;
     private double importoUM;
 //COSTRUTTORE 1
-
     public ContoCorrente() {
         this.id = Banca.reqId();
         this.saldo = 0;
     }
-
-    ;
 //COSTRUTTORE 2
-
     public ContoCorrente(String id) {
         this.id = id;
         this.saldo = 0;
     }
-
     public String getId() {
         return id;
     }
-
     public double getSaldo() {
         return saldo;
     }
-
     public Date getDataUM() {
         return dataUM;
     }
-
     public double getImportoUM() {
         return importoUM;
     }
-
     public double versamento() {
         double importo = Banca.reqImporto();
         this.dataUM = Banca.reqData();
@@ -80,19 +67,16 @@ public class ContoCorrente {
         }
         return -1;
     }
-
     public double versamento(double importo, Date data) {
         this.importoUM = importo;
         this.dataUM = data;
         this.saldo += importo;
         return saldo;
-
     }
-
     public double prelevamento() {
         double importo = Banca.reqImporto();
         this.dataUM = Banca.reqData();
-        if (controlloImporto(Banca.reqImporto(), false)) {
+        if (controlloImporto(importo, false)) {
             this.importoUM = importo;
             this.saldo -= importo;
             return this.saldo;
@@ -101,22 +85,19 @@ public class ContoCorrente {
         }
         return -1;
     }
-
     public double prelevamento(double importo, Date data) {
         this.importoUM = importo;
         this.dataUM = data;
         this.saldo -= importo;
         return this.saldo;
     }
-
     public boolean controlloImporto(double importo, boolean versa) {
         boolean ok;
         if (versa) {
             ok = importo > 0;
-        } else {
-            ok = importo < saldo;
+        } else{
+            ok = importo < saldo && importo>0;
         }
         return ok;
     }
-
 }
